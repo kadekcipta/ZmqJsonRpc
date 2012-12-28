f7b7122acbcd14fad637c27a8f5766d72815669f
Kadek Cipta <kadekcipta@gmail.com>
2012-12-28 16:39:18 +0700

    Add MSA.Test module

---
+++ MSA.Tests/ClientTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MSA.Tests
+{
+    public class ClientTests
+    {
+        [Fact]
+        public void EmptyResponseShouldReturnsError()
+        {
+            Assert.Throws(typeof(NotImplementedException), delegate {
+                throw new NotImplementedException();
+            });
+        }
+
+        [Fact]
+        public void TestHello()
+        {
+            Assert.Equal(100, 20);
+        }
+    }
+}
